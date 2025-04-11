de5727ba4fba925852d8b0dd17e8399767e2ce10
dey12956 <dey12956@gmail.com>
2025-04-11 17:18:18 +0800

    add sys to work with CLI

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ def get_book_text(filepath):
       
 
 def main():
-    frankenstein = get_book_text("./books/frankenstein.txt")
-    num_words = word_count(frankenstein)
-    dict_char_count = get_char_count(frankenstein)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_text = get_book_text(sys.argv[1])
+    num_words = word_count(book_text)
+    dict_char_count = get_char_count(book_text)
     list_dict = list_sort_dict(dict_char_count)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
@@ -19,6 +22,7 @@ def main():
         if d["char"].isalpha():
             print(str(d["char"])+": "+str(d["num"])+"\n")
     print("============= END ===============")
+    
 
     
 
@@ -26,4 +30,5 @@ def main():
 from stats import word_count
 from stats import get_char_count
 from stats import list_sort_dict
+import sys
 main()
