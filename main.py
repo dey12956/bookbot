30ebfa5386c5fd669483c1235185902cfab077be
dey12956 <dey12956@gmail.com>
2025-04-11 17:04:12 +0800

    add list_sort_dict and polish up the report

--- main.py
+++ main.py
@@ -9,10 +9,21 @@ def main():
     frankenstein = get_book_text("./books/frankenstein.txt")
     num_words = word_count(frankenstein)
     dict_char_count = get_char_count(frankenstein)
-    print(f"{num_words} words found in the document")
-    print(dict_char_count)
+    list_dict = list_sort_dict(dict_char_count)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for d in list_dict:
+        if d["char"].isalpha():
+            print(str(d["char"])+": "+str(d["num"])+"\n")
+    print("============= END ===============")
+
+    
 
 
 from stats import word_count
 from stats import get_char_count
+from stats import list_sort_dict
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,11 @@ def get_char_count(file_contents):
         else:
             dict_char_count[char] = 1
     return dict_char_count
+
+def list_sort_dict(dict_char_count):
+    dict_list = []
+    for k, v in dict_char_count.items():
+        new_dict = {"char": k, "num": v}
+        dict_list.append(new_dict)
+    dict_list.sort(reverse=True, key = lambda d:d["num"])
+    return dict_list
